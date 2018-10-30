5e304c6b537a6f51a2678d9555729f67780277bf
Ying Dong <dongyingname@yahoo.com>
2018-10-30 01:58:05 +0000

    finished the functionality. figured out how to used request module. getRepoContributors now extract data and convert it into an node object. downloadImageByURL is the callbackfunction that does the downloading

--- download_avatars.js
+++ download_avatars.js
@@ -1,31 +1,44 @@
 var request = require('request');
 var token = require('./secrets.js');
+var fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader!');
-var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': token
-    }
-  };
-
+var data = {};
 function getRepoContributors(repoOwner, repoName, cb) {
+
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+          'User-Agent': 'request',
+          'Authorization': 'token ' + token.GITHUB_TOKEN
+        }
+      };
     request(options, function(err, res, body) {
-      cb(err, body);
+      data = JSON.parse(body);
+      //console.log(data);
+      data.forEach(function(input){
+        var img = input.avatar_url;
+        var path = './avatars/' + input.login;
+        //console.log(img);
+        cb(img, input, path);
+      })
     });
+    
     // aquire data and convert into js object
+    //return data;
 }
 
-
-
-function downloadImageByURL(url, filePath) {
+function downloadImageByURL(url, input, filePath) {
     // ...
-
-}
-function onRetrieveWeather(error, response, body) {
-    var data = JSON.parse(body);
-};
-getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
-});
+    request.get(url)
+    .on('error',function(err){
+        throw err;
+    })
+    .on('end', function(){ console.log('downloading picture of' + input.login +  'into ' + filePath) })
+    .pipe(fs.createWriteStream(filePath))
+    
+    .on('finish', function(){ console.log('Finished downloading picture of' + input.login +  'into ' + filePath) })
+}   
+// function onRetrieveWeather(error, response, body) {
+//     var data = JSON.parse(body);
+// };
+getRepoContributors("jquery", "jquery", downloadImageByURL);
