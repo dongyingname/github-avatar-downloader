6c8a5e5a6f010ccc8cbdeff3a2eb3fe2dde27e14
Ying Dong <dongyingname@yahoo.com>
2018-10-30 02:25:04 +0000

    beautifullized my code. I also put more comments so that it's easier to read

--- download_avatars.js
+++ download_avatars.js
@@ -1,44 +1,56 @@
+//grab my input <owner> <repo>
+var mine = process.argv.splice(2);
+
+//get the modules and initiate the porject
 var request = require('request');
 var token = require('./secrets.js');
 var fs = require('fs');
+var data = {}; //used for obtaining html that is converted into a node object
 console.log('Welcome to the GitHub Avatar Downloader!');
-var data = {};
-function getRepoContributors(repoOwner, repoName, cb) {
 
-    var options = {
-        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-        headers: {
-          'User-Agent': 'request',
-          'Authorization': 'token ' + token.GITHUB_TOKEN
-        }
-      };
-    request(options, function(err, res, body) {
-      data = JSON.parse(body);
-      //console.log(data);
-      data.forEach(function(input){
-        var img = input.avatar_url;
-        var path = './avatars/' + input.login;
-        //console.log(img);
-        cb(img, input, path);
-      })
-    });
-    
-    // aquire data and convert into js object
-    //return data;
+//get started
+//convert user input into object for the function
+var owner = mine[0];
+var repo = mine[1];
+var urlInfo = {
+	url: "https://api.github.com/repos/" + owner + "/" + repo + "/contributors",
+	headers: {
+		'User-Agent': 'request',
+		'Authorization': 'token ' + token.GITHUB_TOKEN
+	}
 }
+
+//function that convert internet data into node object
+function getRepoContributors(options, cb) {
+	request(options, function (err, res, body) {
+		data = JSON.parse(body); //got the object with names and urls
+
+		data.forEach(function (input) {
+			var img = input.avatar_url;
+			var path = './avatars/' + input.login;
+			cb(img, input, path);
+		})
+	});
+}
+
 //call back function. Added another parameter input which is the name of the contributer
+//consolelog out the status of downloads and download the images into respective folders
 function downloadImageByURL(url, input, filePath) {
-    // ...
-    request.get(url)
-    .on('error',function(err){
-        throw err;
-    })
-    .on('end', function(){ console.log('Downloading picture of' + input.login +  'into ' + filePath) })
-    .pipe(fs.createWriteStream(filePath))
-    
-    .on('finish', function(){ console.log('Finished downloading picture of' + input.login +  'into ' + filePath) })
-}   
-// function onRetrieveWeather(error, response, body) {
-//     var data = JSON.parse(body);
-// };
-getRepoContributors("jquery", "jquery", downloadImageByURL);
+	// ...
+	request.get(url)
+		.on('error', function (err) {
+			throw err;
+		})
+		//start to download
+		.on('end', function () {
+			console.log('Downloading picture of' + input.login + 'into ' + filePath)
+		})
+		.pipe(fs.createWriteStream(filePath))
+		//finished downloading
+		.on('finish', function () {
+			console.log('Finished downloading picture of' + input.login + 'into ' + filePath)
+		})
+}
+
+//execute the program
+getRepoContributors(urlInfo, downloadImageByURL);
